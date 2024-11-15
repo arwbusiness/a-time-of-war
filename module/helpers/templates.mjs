05a7b17eaad45828196a38add16528edf34c3c5a
Roy Williams <arwbusiness@hotmail.com>
2024-11-15 20:08:20 +0800

    next

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -5,8 +5,12 @@
  */
 export const preloadHandlebarsTemplates = async function () {
 	return loadTemplates([
-		'systems/a-time-of-war/templates/actor/parts/PersonActorEquipment.hbs',
 		'systems/a-time-of-war/templates/actor/parts/PersonActorGameplayPC.hbs',
+		'systems/a-time-of-war/templates/actor/parts/PersonActorGameplayNPC.hbs',
+		//'systems/a-time-of-war/templates/actor/parts/PersonActorCharacter.hbs',
+		'systems/a-time-of-war/templates/actor/parts/PersonActorEquipment.hbs',
+		'systems/a-time-of-war/templates/actor/parts/PersonActorProgression.hbs',
+		'systems/a-time-of-war/templates/actor/parts/PersonActorOptions.hbs',
 		/*
 		// Actor partials.
 		'systems/a-time-of-war/templates/actor/parts/actor-features.hbs',
