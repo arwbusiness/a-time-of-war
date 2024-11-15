5356ae33e14df8e5c145f43012e31da331616a64
Roy Williams <arwbusiness@hotmail.com>
2024-11-15 22:00:08 +0800

    asdf

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -1,23 +1,27 @@
 export const BT = {};
 
 /**
- * The set of Ability Scores used within the system.
+ * The set of Attribute Scores used within the system.
  * @type {Object}
  */
-BT.abilities = {
-  str: 'BOILERPLATE.Ability.Str.long',
-  dex: 'BOILERPLATE.Ability.Dex.long',
-  con: 'BOILERPLATE.Ability.Con.long',
-  int: 'BOILERPLATE.Ability.Int.long',
-  wis: 'BOILERPLATE.Ability.Wis.long',
-  cha: 'BOILERPLATE.Ability.Cha.long',
+BT.attributes = {
+  str: 'BT.Attribute.str.long',
+  bod: 'BT.Attribute.bod.long',
+  rfl: 'BT.Attribute.rfl.long',
+  dex: 'BT.Attribute.dex.long',
+  wil: 'BT.Attribute.wil.long',
+  int: 'BT.Attribute.int.long',
+  cha: 'BT.Attribute.cha.long',
+  edg: 'BT.Attribute.edg.long'
 };
 
-BT.abilityAbbreviations = {
-  str: 'BOILERPLATE.Ability.Str.abbr',
-  dex: 'BOILERPLATE.Ability.Dex.abbr',
-  con: 'BOILERPLATE.Ability.Con.abbr',
-  int: 'BOILERPLATE.Ability.Int.abbr',
-  wis: 'BOILERPLATE.Ability.Wis.abbr',
-  cha: 'BOILERPLATE.Ability.Cha.abbr',
+BT.attributeAbbreviations = {
+  str: 'BT.Attribute.str.abbr',
+  bod: 'BT.Attribute.bod.abbr',
+  rfl: 'BT.Attribute.rfl.abbr',
+  dex: 'BT.Attribute.dex.abbr',
+  wil: 'BT.Attribute.wil.abbr',
+  int: 'BT.Attribute.int.abbr',
+  cha: 'BT.Attribute.cha.abbr',
+  edg: 'BT.Attribute.edg.abbr'
 };
